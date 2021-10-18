60f306a06da8711804d762270ff91b51aca18eda
Darling <hi12hello31@gmail.com>
2021-10-18 15:38:42 -0400

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Medida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txttm = new System.Windows.Forms.TextBox();
+            this.txtt = new System.Windows.Forms.TextBox();
+            this.txtmg = new System.Windows.Forms.TextBox();
+            this.txto = new System.Windows.Forms.TextBox();
+            this.txtdg = new System.Windows.Forms.TextBox();
+            this.txtg = new System.Windows.Forms.TextBox();
+            this.txtl = new System.Windows.Forms.TextBox();
+            this.txtkg = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnconvertir = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txttm);
+            this.groupBox1.Controls.Add(this.txtt);
+            this.groupBox1.Controls.Add(this.txtmg);
+            this.groupBox1.Controls.Add(this.txto);
+            this.groupBox1.Controls.Add(this.txtdg);
+            this.groupBox1.Controls.Add(this.txtg);
+            this.groupBox1.Controls.Add(this.txtl);
+            this.groupBox1.Controls.Add(this.txtkg);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(310, 343);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conversión";
+            // 
+            // txttm
+            // 
+            this.txttm.Enabled = false;
+            this.txttm.Location = new System.Drawing.Point(169, 293);
+            this.txttm.Name = "txttm";
+            this.txttm.Size = new System.Drawing.Size(87, 25);
+            this.txttm.TabIndex = 15;
+            // 
+            // txtt
+            // 
+            this.txtt.Enabled = false;
+            this.txtt.Location = new System.Drawing.Point(29, 293);
+            this.txtt.Name = "txtt";
+            this.txtt.Size = new System.Drawing.Size(87, 25);
+            this.txtt.TabIndex = 14;
+            // 
+            // txtmg
+            // 
+            this.txtmg.Enabled = false;
+            this.txtmg.Location = new System.Drawing.Point(169, 224);
+            this.txtmg.Name = "txtmg";
+            this.txtmg.Size = new System.Drawing.Size(87, 25);
+            this.txtmg.TabIndex = 13;
+            // 
+            // txto
+            // 
+            this.txto.Enabled = false;
+            this.txto.Location = new System.Drawing.Point(29, 224);
+            this.txto.Name = "txto";
+            this.txto.Size = new System.Drawing.Size(87, 25);
+            this.txto.TabIndex = 12;
+            // 
+            // txtdg
+            // 
+            this.txtdg.Enabled = false;
+            this.txtdg.Location = new System.Drawing.Point(169, 155);
+            this.txtdg.Name = "txtdg";
+            this.txtdg.Size = new System.Drawing.Size(87, 25);
+            this.txtdg.TabIndex = 11;
+            // 
+            // txtg
+            // 
+            this.txtg.Enabled = false;
+            this.txtg.Location = new System.Drawing.Point(29, 155);
+            this.txtg.Name = "txtg";
+            this.txtg.Size = new System.Drawing.Size(87, 25);
+            this.txtg.TabIndex = 10;
+            // 
+            // txtl
+            // 
+            this.txtl.Enabled = false;
+            this.txtl.Location = new System.Drawing.Point(169, 81);
+            this.txtl.Name = "txtl";
+            this.txtl.Size = new System.Drawing.Size(87, 25);
+            this.txtl.TabIndex = 9;
+            // 
+            // txtkg
+            // 
+            this.txtkg.Location = new System.Drawing.Point(29, 81);
+            this.txtkg.Name = "txtkg";
+            this.txtkg.ShortcutsEnabled = false;
+            this.txtkg.Size = new System.Drawing.Size(87, 25);
+            this.txtkg.TabIndex = 8;
+            this.txtkg.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtkg_KeyPress);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(166, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 17);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Tonelada Métrica";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(26, 265);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 17);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Tonelada";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(166, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 17);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Miligramos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(26, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Onzas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(166, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Decagramos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Gramos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(166, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Libras";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kilogramos";
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(6, 64);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(106, 44);
+            this.btnborrar.TabIndex = 1;
+            this.btnborrar.Text = "Borrar";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(6, 114);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(106, 44);
+            this.btnsalir.TabIndex = 2;
+            this.btnsalir.Text = "Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnborrar);
+            this.groupBox2.Controls.Add(this.btnconvertir);
+            this.groupBox2.Controls.Add(this.btnsalir);
+            this.groupBox2.Location = new System.Drawing.Point(329, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(119, 164);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnconvertir
+            // 
+            this.btnconvertir.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnconvertir.Location = new System.Drawing.Point(6, 14);
+            this.btnconvertir.Name = "btnconvertir";
+            this.btnconvertir.Size = new System.Drawing.Size(106, 44);
+            this.btnconvertir.TabIndex = 3;
+            this.btnconvertir.Text = "Convertir";
+            this.btnconvertir.UseVisualStyleBackColor = true;
+            this.btnconvertir.Click += new System.EventHandler(this.btnconvertir_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 373);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnconvertir;
+        private System.Windows.Forms.TextBox txto;
+        private System.Windows.Forms.TextBox txtdg;
+        private System.Windows.Forms.TextBox txtg;
+        private System.Windows.Forms.TextBox txtl;
+        private System.Windows.Forms.TextBox txtkg;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txttm;
+        private System.Windows.Forms.TextBox txtt;
+        private System.Windows.Forms.TextBox txtmg;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medida
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnconvertir_Click(object sender, EventArgs e)
+        {
+            if (validar())
+            {
+                convertir();
+            }  
+ 
+        }
+
+        void convertir()
+        {
+            //Variables
+            double kilogramos;
+            double libras, gramos, decagramos, onzas, miligramos, tonelada, tonelam;
+            
+            //Entrada
+            kilogramos = Convert.ToDouble(txtkg.Text);
+
+            //Procesos
+            libras = kilogramos * (2.20462);
+            gramos = kilogramos * (1000);
+            decagramos = kilogramos * (100);
+            onzas = kilogramos * (35.2740);
+            miligramos = kilogramos * (1000000);
+            tonelada = kilogramos * (0.001);
+            tonelam = kilogramos * (0.98);
+
+            //Salida
+            txtl.Text = Convert.ToString(libras);
+            txtg.Text = Convert.ToString(gramos);
+            txtdg.Text = Convert.ToString(decagramos);
+            txto.Text = Convert.ToString(onzas);
+            txtmg.Text = Convert.ToString(miligramos);
+            txtt.Text = Convert.ToString(tonelada);
+            txttm.Text = Convert.ToString(tonelam);
+        }
+
+        private void txtkg_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //validacion de un campo para que solo registre numero//
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("Solo numeros", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+            errorProvider1.SetError(txtkg, "");
+
+        }
+
+        private Boolean validar()
+        {
+            Boolean ok = true;
+            
+            if(txtkg.Text == "")
+            {
+                ok = false;
+                errorProvider1.SetError(txtkg, "Ingresar cantidad de kilogramos");
+            }
+            return ok;
+
+            
+        }
+    }
+}
